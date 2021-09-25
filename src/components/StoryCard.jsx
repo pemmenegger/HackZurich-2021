b6198336793d57d6f94945b6f52ae87537051767
Technology <technology@moonandstars.ch>
2021-09-25 22:48:18 +0200

    clean create react app, tailwind

--- src/components/StoryCard.jsx
+++ src/components/StoryCard.jsx
@@ -1,54 +1,31 @@
-import React from 'react';
-
-import ConfirmAlert from './context/foreground/ConfirmAlert.jsx';
-import { openSuccessAlert } from '../rigiUtils.js';
-import { withForegroundContext } from "./context/ForegroundContext"
-import { finishNote } from "../services/notesService"
+import React from "react";
 
 class StoryCard extends React.Component {
-	constructor() {
-		super();
-    }
+  constructor() {
+    super();
+  }
 
-    handleFinishNote() {
-        let confirmOnClick = async () => {
-            try {
-                await finishNote(this.props.SessionSettingsContext.rigiUser);
-                openSuccessAlert(this.props.foregroundContext, "Auftrag wurde erledigt");
-            } catch (e) {
-                openErrorAlert(this.props.foregroundContext, "Auftrag konnte nicht erledigt werden");
-            }
-        }
-
-        this.props.foregroundContext.openPopup(
-            <ConfirmAlert 
-                closePopup={this.props.foregroundContext.closePopup}
-                title="Auftrag erledigt?"
-                message="Bei Erledigung des Auftrags wird er ins Archiv gelegt."
-                confirmLabel="Erledigt"
-                confirmOnClick={confirmOnClick}
-            />
-        );
-    }
-
-	render() {
-		return (
-			<div className="story-card">
-                    <div className="wrapper" style={{  
-                    backgroundImage: "url(" + this.props.imgUrl + ")",
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                }}>
-                    <div className="overlay"></div>
-                    <div className="headlines">
-                        <p className="subheadline">{this.props.subheadline}</p>
-                        <p className="headline">{this.props.headline}</p>
-                    </div>
-                </div>
-            </div>
-		);
-	}
+  render() {
+    return (
+      <div className="story-card">
+        <div
+          className="wrapper"
+          style={{
+            backgroundImage: "url(" + this.props.imgUrl + ")",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="overlay"></div>
+          <div className="headlines">
+            <p className="subheadline">{this.props.subheadline}</p>
+            <p className="headline">{this.props.headline}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withForegroundContext(StoryCard);
+export default StoryCard;
---
+++ src/pages/Article.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <>
+      <div className="w-full h-screen bg-black">test</div>
+    </>
+  );
+};
+
+export default Article;
